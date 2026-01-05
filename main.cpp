5515430cc1fe43e570c85cdce1e1b4a2699aa8f9
Issa Oulkhir <ioulkhir@e2r7p5.1337.ma>
2026-01-05 11:18:32 +0100

    comment stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 
 
+
+/*
+ *
+ * First we Parse the config file
+ * Then we setup services listening on diff ports
+ * Then each service handles its request
+ * 	CGI {Python3, fast-cgi}
+ * Done
+ *
+ * */
+
 #include "inc/webserv.hpp"
 
 int main(int ac, char **av)
